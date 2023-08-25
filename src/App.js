c237cbb70554485750db39ed125f4938388563b6
박은성 <cro3179@naver.com>
2023-08-25 13:08:50 +0900

    파일변경

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           파일변경
         </p>
         <a
           className="App-link"
